2ab92c38e2d63557a5578a4f385a7be7d5d7f631
BuildTools <unconfigured@null.spigotmc.org>
2019-06-27 15:05:58 -0700

    more code

--- main.py
+++ main.py
@@ -7,13 +7,37 @@ screen_info = pygame.display.Info()
 
 size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h))
 
-clock = pygame.time.Clock()
-
 background = pygame.image.load('background.png')
 background = pygame.transform.smoothscale(background, (width, height))
 
+startPos = (width/8, height/2)
+pipes = pygame.sprite.Group
+player = bird(startPos)
+gapSize = random.randint(200,300)
+loopCount = 0
+color = (0,0,0)
+screen = pygame.display.set_mode(size)
+
+def lose():
+    font = pygame.font.SysFont(None, 70)
+    text = font.render("You Failed", True, (0,0,255))
+    text_rect = text.get_rect()
+    text_rect.center = (width/2, height/2)
+    while True:
+        screen.fill(color)
+        screen.blit(text, text_rect)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    player.reset()
+                    pipes.empty()
+                    return
+
 def main():
     pass
+
+lose()
 
 if __name__ == '__main__':
     main()
--- pipe.py
+++ pipe.py
@@ -1,8 +1,19 @@
-import pygame
+import pygame, random
 
 class pipe(pygame.sprite.Sprite):
-    def __init__(self, pos):
+    def __init__(self, pos, isTop = False):
+        super().__init__()
         self.image = pygame.image.load('pipe.png')
         self.image = pygame.transform.smoothscale(self.image, (40, 40))
 
-        self.rect = pygame.image.get_rect()
+        if isTop:
+            self.image = pygame.transform.rotate(self.image, -180)
+
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+        self.speed = pygame.math.Vector2(-8, 0)
+
+    def update(self):
+        self.rect.move_ip(self.speed)
+
+#test = pipe((0,0))
